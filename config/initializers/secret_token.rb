7fe6ed9336802564e257835fc026c0771d74e606
Andrew Hubbs <andrewhubbs@Andrew-Hubbss-MacBook-Pro.local>
2011-09-21 09:48:09 -0700

    getting repo working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Signin::Application.config.secret_token = '6e3eba142f7554a3fc27d2efd6e994d213fb6be0cc1cfcff0e46a05bb52335047336d59a5d79bd82300c5db93017fd66d38f880d80672b2368bb8e5cf95ab7b3'
